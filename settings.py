f6590453251a120070b511c81cff5fc7e68d31ff
TBersoux <tristanbersoux@gmail.com>
2019-10-18 19:22:48 +0200

    main et gui crées, le labyrinthe s'affiche dans une fenêtre et il est possible d'en génerer de nouveaux

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 def init(): #!do not forget to call in main !
     global COLUMNS
     global ROWS
-    COLUMNS = 3
-    ROWS = 3
+    global BOXSIDE
+    COLUMNS = int(input("How many columns (Default: 30) ? ") or "30")
+    ROWS = int(input("How many rows (Default : 30) ? ") or "30")
+    BOXSIDE = 20
