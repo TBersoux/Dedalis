9c99a360838e7070bf6a3144ed6f0b871e2836e1
TBersoux <tristanbersoux@gmail.com>
2020-03-24 23:24:43 +0100

    Changement de langage pour pousser l'optimisation et passer sur OpenGL

--- globals.py
+++
@@ -1,9 +0,0 @@
-def init(): 
-    global COLUMNS
-    global ROWS
-    global BOXSIDE
-    BOXSIDE = 20
-    COLUMNS = 40
-    ROWS = 40
-    global fini
-    fini = False #TODO: Mieux référencer évidemment, dans players
